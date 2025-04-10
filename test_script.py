df9bd3e36e670d8fe35803218a9be10bb9848335
Олег <olegkovalets1112003@knu.ua>
2025-04-10 20:42:29 +0300

    add test script to review

---
+++ test_script.py
@@ -0,0 +1,18 @@
+# test_script.py
+
+a = 5
+b = 10
+c = a + b
+print("Result is", c)
+
+def calculateSomething(x, y):
+    return x * y # multiplies numbers
+
+result = calculateSomething(3, 7)
+print("Calculated result is:", result)
+
+long_variable_name = "This is a very very very very very very very very very very long string that exceeds the limit"
+
+d = 100
+e = d / 5
+print(e)
